a9b58d85b8ba5ab1af53c2bd8251dde7efce4e8c
David Vesely <veselda7@gmaill.com>
2017-11-20 22:30:36 +0100

    Not working state of error handling.

--- calculator/api.py
+++ calculator/api.py
@@ -1,17 +1,25 @@
-from calculator.talibWrapper import allTalibFunctions
-import json
+from calculator.talibWrapper import allTalibFunctions, functionInfo
+from falcon import HTTPError
 
 
 def availableFunctions():
-    talibFuncNames = ['TALIB-%s' % name for name in allTalibFunctions()]
-    jsonData = json.dumps({
-        'names': talibFuncNames
-    })
+    jsonData = {
+        'TALIB': allTalibFunctions()
+    }
     return jsonData
 
 
-def functionInfo():
-    raise
+def functionInfo(group, functionName):
+    if group == 'TALIB':
+        try:
+            fInfo = functionInfo(functionName)
+            return {functionName: fInfo}
+        except:
+            message = "Function '{fName}' in group '{group}' does not exist.".format(fname=functionName, group=group)
+            raise HTTPError(message, code=501)
+
+    message = "Group '{group}' does not exist.".format(group=group)
+    raise HTTPError(message, code=501)
 
 
 def calculateData():
--- calculator/talibWrapper.py
+++ calculator/talibWrapper.py
@@ -4,3 +4,8 @@ from talib import get_functions
 
 def allTalibFunctions():
     return get_functions()
+
+
+def functionInfo(functionName):
+    info = abstract.Function(functionName)
+    return str(info)
