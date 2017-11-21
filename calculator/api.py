382714194f6b9f3e2603e21691969457d3f834b0
David Vesely <veselda7@gmaill.com>
2017-11-21 21:21:52 +0100

    Implemented first error handling.

--- calculator/api.py
+++ calculator/api.py
@@ -1,26 +1,38 @@
-from calculator.talibWrapper import allTalibFunctions, functionInfo
-from falcon import HTTPError
+from calculator.talibWrapper import allTalibFunctions, talibFunctionInfo
+import calculator.talibWrapper as talibW
+from enum import Enum
+
+
+class FunctionGroups(Enum):
+    TALIB = 'TALIB'
+
+    @staticmethod
+    def toList():
+        return list(map(lambda c: c.value, FunctionGroups))
 
 
 def availableFunctions():
     jsonData = {
-        'TALIB': allTalibFunctions()
+        FunctionGroups.TALIB.value: allTalibFunctions()
     }
     return jsonData
 
 
 def functionInfo(group, functionName):
-    if group == 'TALIB':
-        try:
-            fInfo = functionInfo(functionName)
-            return {functionName: fInfo}
-        except:
-            message = "Function '{fName}' in group '{group}' does not exist.".format(fname=functionName, group=group)
-            raise HTTPError(message, code=501)
-
-    message = "Group '{group}' does not exist.".format(group=group)
-    raise HTTPError(message, code=501)
+    if group == FunctionGroups.TALIB.value:
+        fInfo = talibFunctionInfo(functionName)
+        return {functionName: fInfo}
 
 
 def calculateData():
     raise
+
+
+def isGroupAndNameValid(group, functionName):
+    if group not in FunctionGroups.toList():
+        return False, "Group {group} is not implemented.".format(**locals())
+
+    if not talibW.isFunctionValid(functionName):
+        return False, "Function {functionName} of group {group} is not implemented.".format(**locals())
+
+    return True
--- calculator/talibWrapper.py
+++ calculator/talibWrapper.py
@@ -6,6 +6,10 @@ def allTalibFunctions():
     return get_functions()
 
 
-def functionInfo(functionName):
+def isFunctionValid(functionName):
+    return functionName in allTalibFunctions()
+
+
+def talibFunctionInfo(functionName):
     info = abstract.Function(functionName)
     return str(info)
