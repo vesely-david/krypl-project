272ed281f9f4963e43a26cde4ec9b74fd07d549e
vesely <david.vesely@jumpshot.com>
2018-03-23 16:44:27 +0100

    Plotting candles using matplotlib and its notebook magic.

--- notebooks/utils/common.py
+++ notebooks/utils/common.py
@@ -1,5 +1,8 @@
-import time
 import datetime as dt
+from matplotlib.finance import candlestick2_ohlc
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator, FuncFormatter
+import datetime as datetime
 
 
 def timestampToDate(timestamp):
@@ -8,3 +11,22 @@ def timestampToDate(timestamp):
 
 def strTimeToTimestamp(strTime):
     return dt.datetime.strptime(strTime, "%Y-%m-%d %H:%M:%S").timestamp()
+
+
+def plotCandles(ohlc):
+    def getDate(x, _):
+        try:
+            return xdate[int(x)]
+        except IndexError:
+            return ''
+
+    fig, ax = plt.subplots(figsize=(9, 6))
+    candlestick2_ohlc(ax, ohlc['open'], ohlc['high'], ohlc['low'], ohlc['close'], width=0.6)
+
+    xdate = [datetime.datetime.fromtimestamp(i) for i in ohlc['timestamp']]
+    ax.xaxis.set_major_locator(MaxNLocator(6))
+    ax.xaxis.set_major_formatter(FuncFormatter(getDate))
+
+    fig.autofmt_xdate()
+    fig.tight_layout()
+    plt.show()
