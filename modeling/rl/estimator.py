411dd3bc0191e503d5288c158410960a1c93ac28
vesely <david.vesely@jumpshot.com>
2018-05-11 16:16:34 +0200

    Rename fields.

--- modeling/rl/estimator.py
+++ modeling/rl/estimator.py
@@ -38,63 +38,63 @@ class TensorflowEstimator:
 
 class MLPPolicyEstimator(PolicyEstimator, TensorflowEstimator):
 
-    def __init__(self, numOfFeatures, numOfOutputs, hidden_dims, learningRate=0.01):
+    def __init__(self, input_dim, output_dim, hidden_dims, learning_rate=0.01):
         with tf.variable_scope("perceptron_policy_estimator"):
-            self.state = tf.placeholder(tf.float32, [None, numOfFeatures], "state")
+            self.state = tf.placeholder(tf.float32, [None, input_dim], "state")
             self.action = tf.placeholder(dtype=tf.int32, name="action")
             self.target = tf.placeholder(dtype=tf.float32, name="target")
 
             hidden = self.add_hidden(self.state, hidden_dims)
 
-            self.outputLayer = tf.contrib.layers.fully_connected(
+            self.output_layer = tf.contrib.layers.fully_connected(
                 inputs=hidden,
-                num_outputs=numOfOutputs,
+                num_outputs=output_dim,
                 activation_fn=None,
                 weights_initializer=tf.zeros_initializer)
 
-            self.actionProbs = tf.squeeze(tf.nn.softmax(self.outputLayer))
-            self.pickedActionProb = tf.gather(self.actionProbs, self.action)
-            self.loss = -tf.log(self.pickedActionProb) * self.target
-            self.optimizer = tf.train.AdamOptimizer(learning_rate=learningRate)
-            self.trainOp = self.optimizer.minimize(self.loss, global_step=tf.train.get_global_step())
+            self.action_probs = tf.squeeze(tf.nn.softmax(self.output_layer))
+            self.picked_action_prob = tf.gather(self.action_probs, self.action)
+            self.loss = -tf.log(self.picked_action_prob) * self.target
+            self.optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+            self.train_op = self.optimizer.minimize(self.loss, global_step=tf.train.get_global_step())
 
     def predict(self, state):
         sess = tf.get_default_session()
-        return sess.run(self.actionProbs, {self.state: state})
+        return sess.run(self.action_probs, {self.state: state})
 
     def update(self, state, target, action):
         sess = tf.get_default_session()
-        feedDict = {self.state: state, self.target: target, self.action: action}
-        _, loss = sess.run([self.trainOp, self.loss], feedDict)
+        feed_dict = {self.state: state, self.target: target, self.action: action}
+        _, loss = sess.run([self.train_op, self.loss], feed_dict)
         return loss
 
 
 class MLPValueEstimator(ValueEstimator, TensorflowEstimator):
-    def __init__(self, input_dim, hidden_dims=[], learningRate=0.1):
+    def __init__(self, input_dim, hidden_dims=[], learning_rate=0.1):
         with tf.variable_scope("perceptron_value_estimator"):
             self.state = tf.placeholder(tf.float32, [None, input_dim], "state")
             self.target = tf.placeholder(dtype=tf.float32, name="target")
 
             hidden = self.add_hidden(self.state, hidden_dims)
 
-            self.outputLayer = tf.contrib.layers.fully_connected(
+            self.output_layer = tf.contrib.layers.fully_connected(
                 inputs=hidden,
                 num_outputs=1,
                 activation_fn=None,
                 weights_initializer=tf.zeros_initializer
             )
 
-            self.valueEstimate = tf.squeeze(self.outputLayer)
-            self.loss = tf.squared_difference(self.valueEstimate, self.target)
-            self.optimizer = tf.train.AdamOptimizer(learning_rate=learningRate)
+            self.value_estimate = tf.squeeze(self.output_layer)
+            self.loss = tf.squared_difference(self.value_estimate, self.target)
+            self.optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
             self.trainOp = self.optimizer.minimize(self.loss, global_step=tf.train.get_global_step())
 
     def predict(self, state):
         sess = tf.get_default_session()
-        return sess.run(self.valueEstimate, {self.state: state})
+        return sess.run(self.value_estimate, {self.state: state})
 
     def update(self, state, target):
         sess = tf.get_default_session()
-        feedDict = {self.state: state, self.target: target}
-        _, loss = sess.run([self.trainOp, self.loss], feedDict)
+        feed_dict = {self.state: state, self.target: target}
+        _, loss = sess.run([self.trainOp, self.loss], feed_dict)
         return loss
