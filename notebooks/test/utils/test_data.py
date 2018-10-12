c124b9806cc15fb06d0ae0f59c3406b2ca012a8e
David Vesely <veselda7@gmaill.com>
2018-10-12 20:45:36 +0200

    Test ModelingData.

--- notebooks/test/utils/test_data.py
+++ notebooks/test/utils/test_data.py
@@ -1,10 +1,43 @@
 from unittest import TestCase
 from utils.data import ModelingData
+import pandas as pd
 
 
 class TestData(TestCase):
 
-    def test_split(self):
-        data = pd.DataFrame({
-            'timestamp': list(range(100))
+    def setUp(self):
+        df = pd.DataFrame({
+            'timestamp': list(range(102)),
+            'date': [f'{y}-01-01' for y in range(2000, 2102)],
+            'label': [0] * 102,
+            'f1': [10] * 102,
+            'f2': [20] * 102
         })
+
+        self.modeling_data = ModelingData(df, ['timestamp', 'date', 'label'], '2001-01-01', '2101-01-01')
+
+    def test_X_has_only_feature_cols(self):
+        self.assertListEqual(list(self.modeling_data.X_train.columns), ['f1', 'f2'])
+        self.assertListEqual(list(self.modeling_data.X_val.columns), ['f1', 'f2'])
+        self.assertListEqual(list(self.modeling_data.X_test.columns), ['f1', 'f2'])
+
+    def test_train_sizes(self):
+        X_size = len(self.modeling_data.X_train)
+        y_size = len(self.modeling_data.y_train)
+        self.assertEqual(X_size, 49)
+        self.assertEqual(y_size, 49)
+
+    def test_validation_sizes(self):
+        X_size = len(self.modeling_data.X_val)
+        y_size = len(self.modeling_data.y_val)
+        self.assertEqual(X_size, 21)
+        self.assertEqual(y_size, 21)
+
+    def test_test_size(self):
+        X_size = len(self.modeling_data.X_test)
+        y_size = len(self.modeling_data.y_test)
+        self.assertEqual(X_size, 30)
+        self.assertEqual(y_size, 30)
+
+    def test_strategy_testing_data(self):
+        self.assertEqual(self.modeling_data.strategy_testing_data['date'].min(), '2101-01-01')
--- notebooks/utils/data.py
+++ notebooks/utils/data.py
@@ -4,25 +4,21 @@ from utils.common import reset_index_hard, write_tsv
 
 class ModelingData:
     def __init__(self, data, columns_to_drop, modeling_start, strategy_testing_start):
-        def split_trading_data():
-            self.strategy_testing_data = self.filter_df(f'date >= "{self.strategy_testing_start}"')
-            self.modeling_data = self.filter_df(f'"{self.modeling_start}" <= date < "{self.strategy_testing_start}"')
-
-            X = self.drop_columns(self.modeling_data, self.columns_to_drop)
-            y = self.modeling_data['label'].apply(int)
-            self.X_train, self.X_val, self.X_test, self.y_train, self.y_val, self.y_test = \
-                self.train_validation_test_split(X, y)
-
         self.data = data
         self.columns_to_drop = columns_to_drop
         self.modeling_start = modeling_start
         self.strategy_testing_start = strategy_testing_start
-        split_trading_data()
+        self.strategy_testing_data = None
+        self.modeling_data = None
+        self.X_train, self.X_val, self.X_test = None, None, None
+        self.y_train, self.y_val, self.y_test = None, None, None
+
+        self.split_trading_data()
 
     @staticmethod
     def train_validation_test_split(X, y):
-        X_trainval, X_test, y_trainval, y_test = train_test_split(X, y, test_size=0.33)
-        X_train, X_val, y_train, y_val = train_test_split(X_trainval, y_trainval, test_size=0.33)
+        X_trainval, X_test, y_trainval, y_test = train_test_split(X, y, test_size=0.3)
+        X_train, X_val, y_train, y_val = train_test_split(X_trainval, y_trainval, test_size=0.3)
         return X_train, X_val, X_test, y_train, y_val, y_test
 
     @staticmethod
@@ -38,6 +34,15 @@ class ModelingData:
             .copy() \
             .sort_values('timestamp')
         return reset_index_hard(res)
+
+    def split_trading_data(self):
+        self.strategy_testing_data = self.filter_data(f'date >= "{self.strategy_testing_start}"')
+        self.modeling_data = self.filter_data(f'"{self.modeling_start}" <= date < "{self.strategy_testing_start}"')
+
+        X = self.drop_columns(self.modeling_data, self.columns_to_drop)
+        y = self.modeling_data['label'].apply(int)
+        self.X_train, self.X_val, self.X_test, self.y_train, self.y_val, self.y_test = \
+            self.train_validation_test_split(X, y)
 
     def write_split(self, root):
         write_tsv(self.X_train, f"{root}/X_train.tsv")
